7fdd788d0f92c80320a20193ce6d70fceacfb039
Given Ncube <givenyslim12@gmail.com>
2024-10-04 01:36:05 +0200

    refactor: used stimulus classes api

--- src/controllers/customize_controller.js
+++ src/controllers/customize_controller.js
@@ -3,38 +3,34 @@ import { Controller } from "@hotwired/stimulus";
 export default class extends Controller {
   static targets = ["leftFrame", "rightFrame", "topFrame", "bottomFrame"];
 
-  FRAME_HIGHLITED_CLASSES = [
-    "bg-orange-500",
-    "border-orange-500",
-    "text-white",
+  static classes = [
+    "activeFrame",
+    "activeFrameChildren",
+    "defaultFrame",
+    "defaultFrameChildren",
   ];
-
-  FRAME_CHILDREN_ACTIVE_CLASSES = ["bg-orange-500", "text-white"];
-
-  FRAME_DEFAULT_CLASSES = ["bg-gray-500"];
-  FRAME_CHILDREN_DEFAULT_CLASSES = ["bg-gray-200"];
 
   highlight({ target, params }) {
     const targetElement = this[`${params.target}Target`];
-    targetElement.classList.remove(...this.FRAME_DEFAULT_CLASSES);
+    targetElement.classList.remove(...this.defaultFrameClasses);
     targetElement.firstElementChild.classList.remove(
-      ...this.FRAME_CHILDREN_DEFAULT_CLASSES,
+      ...this.defaultFrameChildrenClasses,
     );
-    targetElement.classList.add(...this.FRAME_HIGHLITED_CLASSES);
+    targetElement.classList.add(...this.activeFrameClasses);
     targetElement.firstElementChild.classList.add(
-      ...this.FRAME_CHILDREN_ACTIVE_CLASSES,
+      ...this.activeFrameChildrenClasses,
     );
   }
 
   unhighlight({ target, params }) {
     const targetElement = this[`${params.target}Target`];
-    targetElement.classList.remove(...this.FRAME_HIGHLITED_CLASSES);
+    targetElement.classList.remove(...this.activeFrameClasses);
     targetElement.firstElementChild.classList.remove(
-      ...this.FRAME_CHILDREN_ACTIVE_CLASSES,
+      ...this.activeFrameChildrenClasses,
     );
-    targetElement.classList.add(...this.FRAME_DEFAULT_CLASSES);
+    targetElement.classList.add(...this.defaultFrameClasses);
     targetElement.firstElementChild.classList.add(
-      ...this.FRAME_CHILDREN_DEFAULT_CLASSES,
+      ...this.defaultFrameChildrenClasses,
     );
   }
 }
