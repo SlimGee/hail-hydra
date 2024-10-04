fbc3145269cd0dae103f4900c38906de632a0813
Given Ncube <givenyslim12@gmail.com>
2024-10-04 02:51:16 +0200

    feat: added the ability to update frame values on input change

--- assets/application.js
+++ assets/application.js
@@ -2866,14 +2866,80 @@
 
   // src/controllers/customize_controller.js
   var customize_controller_default = class extends Controller {
-    static targets = ["leftFrame", "rightFrame", "topFrame", "bottomFrame"];
+    static targets = [
+      "leftFrame",
+      "rightFrame",
+      "topFrame",
+      "bottomFrame",
+      "leftFrameInput",
+      "rightFrameInput",
+      "topFrameInput",
+      "bottomFrameInput",
+    ];
     static classes = [
       "activeFrame",
       "activeFrameChildren",
       "defaultFrame",
       "defaultFrameChildren",
     ];
-    highlight({ target, params }) {
+    static values = {
+      leftFrame: Number,
+      rightFrame: Number,
+      topFrame: Number,
+      bottomFrame: Number,
+    };
+    initialize() {
+      Object.keys(this.constructor.values).forEach((value) => {
+        if (this[`${value}ValueChanged`]) return;
+        this[`${value}ValueChanged`] = function (newValue) {
+          this[`${value}InputTarget`].value = newValue;
+          const targetElement = this[`${value}Target`];
+          targetElement.firstElementChild.innerHTML = `${newValue}mm`;
+        };
+      });
+    }
+    /**
+     * Handle frame input change events
+     * update the corresponding value and show an error message if the value is less than 1
+     *
+     * @param {Event} event
+     * @returns {void}
+     */
+    frameInputChanged({ params }) {
+      const value = Number(this[`${params.target}InputTarget`].value);
+      this[`${params.target}InputTarget`].classList.remove("border-red-500");
+      const errorMessage =
+        this[`${params.target}InputTarget`].nextElementSibling;
+      if (errorMessage) errorMessage.remove();
+      if (value < 1) {
+        this.showError(
+          this[`${params.target}InputTarget`],
+          "Please enter a value greater than 0",
+        );
+        return;
+      }
+      this[`${params.target}Value`] = this[`${params.target}InputTarget`].value;
+    }
+    /**
+     * Show an error message below the input
+     *
+     * @param {HTMLElement} element
+     * @param {string} message
+     * @returns {void}
+     */
+    showError(element, message) {
+      element.classList.add("border-red-500");
+      const span = document.createElement("span");
+      span.classList.add("text-red-500", "text-xl", "block");
+      span.innerHTML = message;
+      element.parentNode.appendChild(span);
+    }
+    /**
+     * Highlight the frame when the corresponding input is focused
+     *
+     * @param {Event} event
+     */
+    highlight({ params }) {
       const targetElement = this[`${params.target}Target`];
       targetElement.classList.remove(...this.defaultFrameClasses);
       targetElement.firstElementChild.classList.remove(
@@ -2884,6 +2950,11 @@
         ...this.activeFrameChildrenClasses,
       );
     }
+    /**
+     * Unhighlight the frame when the corresponding input is blurred
+     *
+     * @param {Event} event
+     */
     unhighlight({ target, params }) {
       const targetElement = this[`${params.target}Target`];
       targetElement.classList.remove(...this.activeFrameClasses);
--- src/controllers/customize_controller.js
+++ src/controllers/customize_controller.js
@@ -1,7 +1,16 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["leftFrame", "rightFrame", "topFrame", "bottomFrame"];
+  static targets = [
+    "leftFrame",
+    "rightFrame",
+    "topFrame",
+    "bottomFrame",
+    "leftFrameInput",
+    "rightFrameInput",
+    "topFrameInput",
+    "bottomFrameInput",
+  ];
 
   static classes = [
     "activeFrame",
@@ -10,7 +19,77 @@ export default class extends Controller {
     "defaultFrameChildren",
   ];
 
-  highlight({ target, params }) {
+  static values = {
+    leftFrame: Number,
+    rightFrame: Number,
+    topFrame: Number,
+    bottomFrame: Number,
+  };
+
+  initialize() {
+    // Register dynamic event handler for value changes
+    Object.keys(this.constructor.values).forEach((value) => {
+      if (this[`${value}ValueChanged`]) return;
+
+      this[`${value}ValueChanged`] = function (newValue) {
+        this[`${value}InputTarget`].value = newValue;
+
+        const targetElement = this[`${value}Target`];
+        targetElement.firstElementChild.innerHTML = `${newValue}mm`;
+      };
+    });
+  }
+
+  /**
+   * Handle frame input change events
+   * update the corresponding value and show an error message if the value is less than 1
+   *
+   * @param {Event} event
+   * @returns {void}
+   */
+  frameInputChanged({ params }) {
+    const value = Number(this[`${params.target}InputTarget`].value);
+
+    // Reset the error message
+    this[`${params.target}InputTarget`].classList.remove("border-red-500");
+    const errorMessage = this[`${params.target}InputTarget`].nextElementSibling;
+    if (errorMessage) errorMessage.remove();
+
+    //show error message if value is less than 1
+    if (value < 1) {
+      this.showError(
+        this[`${params.target}InputTarget`],
+        "Please enter a value greater than 0",
+      );
+
+      return;
+    }
+
+    // Update the corresponding value in state
+    this[`${params.target}Value`] = this[`${params.target}InputTarget`].value;
+  }
+
+  /**
+   * Show an error message below the input
+   *
+   * @param {HTMLElement} element
+   * @param {string} message
+   * @returns {void}
+   */
+  showError(element, message) {
+    element.classList.add("border-red-500");
+    const span = document.createElement("span");
+    span.classList.add("text-red-500", "text-xl", "block");
+    span.innerHTML = message;
+    element.parentNode.appendChild(span);
+  }
+
+  /**
+   * Highlight the frame when the corresponding input is focused
+   *
+   * @param {Event} event
+   */
+  highlight({ params }) {
     const targetElement = this[`${params.target}Target`];
     targetElement.classList.remove(...this.defaultFrameClasses);
     targetElement.firstElementChild.classList.remove(
@@ -22,6 +101,11 @@ export default class extends Controller {
     );
   }
 
+  /**
+   * Unhighlight the frame when the corresponding input is blurred
+   *
+   * @param {Event} event
+   */
   unhighlight({ target, params }) {
     const targetElement = this[`${params.target}Target`];
     targetElement.classList.remove(...this.activeFrameClasses);
